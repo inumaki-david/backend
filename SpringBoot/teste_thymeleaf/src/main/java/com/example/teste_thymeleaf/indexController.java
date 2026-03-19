f7c159bc0e260d8b4e79d27fe35b90f210f135a9
inumaki-david <davibrennam2008@gmail.com>
2026-03-19 16:53:08 -0300

    backend

--- SpringBoot/teste_thymeleaf/src/main/java/com/example/teste_thymeleaf/indexController.java
+++ SpringBoot/teste_thymeleaf/src/main/java/com/example/teste_thymeleaf/indexController.java
@@ -1,6 +1,5 @@
 package com.example.teste_thymeleaf;
 
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class indexController {
