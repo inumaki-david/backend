ccacf31e527c7a5e4df4e01b6b7692224ab7ab21
inumaki-david <davibrennam2008@gmail.com>
2026-04-09 09:42:22 -0300

    continuando api star wars

--- API's/StarWars_API/api_starwars.js
+++ API's/StarWars_API/api_starwars.js
@@ -1,5 +1,6 @@
 //importar o prompt sync
 const prompt = require('prompt-sync')();
+const axios = require('axios');
 
 //função para menu
 function menu() {
@@ -11,22 +12,75 @@ function menu() {
     console.log("5. Consultar Veículos.")
     console.log("6. Consultar Naves Estelares.")
     console.log("7. Sair da API.")
+    console.log("----------------------------------")
 }
 
-//função principal para consultar os personagens de Star Wars
+//função para consultar os personagens de Star Wars
+async function consultarPersonagem () {
+    const id = prompt("Digite o ID do Personagem (ex: 1 para Luke): ");
+    const url = `https://swapi.dev/api/people/${id}`;
+
+    try {
+        console.log("\nExplorando a Galáxia...");
+
+        const resposta = await axios.get(url);
+        const dados = resposta.data;
+        
+        console.log("\n=== Resultado Encontrado ===");
+        console.log(`Nome: ${dados.name}`);
+        console.log(`Altura: ${dados.height}cm`);
+        console.log(`Massa Corporal: ${dados.mass}`);
+        console.log(`Cor do Cabelo: ${dados.hair_color}`);
+        console.log(`Cor da Pele: ${dados.skin_color}`);
+        console.log(`Cor dos Olhos: ${dados.eye_color}`);
+        console.log(`Ano de Nascimento: ${dados.birth_year}`);
+        console.log(`Gênero: ${dados.gender}`);
+        console.log("----------------------------");
+    } catch (erro) {
+        console.log("[Erro]: ID do Personagem não encontrado. Tente novamente.")
+    }
+}
+
+//função para consultar os planetas 
+async function consultarPlaneta() {
+    const id = prompt("Digite o ID do Planeta (ex: 1 para Tatooine): ");
+    const url = `https://swapi.dev/api/planets/${id}`;
+
+    try {
+        console.log("\nVerificando os Planetas...");
+
+        const resposta = await axios.get(url);
+        const dados = resposta.data;
+
+        console.log("\n=== Resultado Encontrado ===");
+        console.log(`Planeta: ${dados.name}`);
+        console.log(`Período de Rotação: ${dados.rotation_period}`);
+        console.log(`Período Orbital: ${dados.orbital_period}`);
+        console.log(`Diâmetro: ${dados.diameter}`);
+        console.log(`Clima: ${dados.climate}`);
+        console.log(`Gravidade: ${dados.gravity}`);
+        console.log(`Terreno: ${dados.terrain}`);
+        console.log(`Porcentagem de Água na Superfície: ${dados.surface_water}`);
+        
+    } catch (erro) {
+        console.log("[Erro]: ID do Planeta não encontrado. Tente novamente");
+    }
+}
+
 
 //função principal main
-function main () {
+async function main () {
     let escolha;
     do {
         menu();
-        escolha = prompt("\nEscolha uma opção: ");
+
+        escolha = prompt("Escolha uma opção: ");
         switch (escolha) {
             case "1":
-                //aqui vai ficar a função para os personagens
+                await consultarPersonagem();
                 break;
             case "2":
-                //aqui vai ficar a função para os planetas
+                await consultarPlaneta();
                 break;
             case "3":
                 //aqui vai ficar a função para os filmes
@@ -41,10 +95,10 @@ function main () {
                 //aqui vai ficar a função para as naves 
                 break;
             case "7":
-                console.log("Saindo da API  de Star Wars...")
+                console.log("\nSaindo da API  de Star Wars... Que a força esteja com você!")
                 break;
             default: 
-                console.log("\nEscolha Inválida.")
+                console.log("\n[Erro]: Escolha Inválida.")
         }
     } while (escolha != 7);
 }
