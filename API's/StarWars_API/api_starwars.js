6bd7682c28889d4b79e170debf80b2a57dd95608
inumaki-david <davibrennam2008@gmail.com>
2026-04-09 16:45:37 -0300

    continuando projeto

--- API's/StarWars_API/api_starwars.js
+++ API's/StarWars_API/api_starwars.js
@@ -61,19 +61,86 @@ async function consultarPlaneta() {
         console.log(`Gravidade: ${dados.gravity}`);
         console.log(`Terreno: ${dados.terrain}`);
         console.log(`Porcentagem de Água na Superfície: ${dados.surface_water}`);
+        console.log("--------------------------------------");
         
     } catch (erro) {
         console.log("[Erro]: ID do Planeta não encontrado. Tente novamente.");
     }
 }
 
+//função para consultar os filmes
+async function consultarFilmes() {
+    const id = prompt("Digite o ID do Filme (ex: 2 para The Empire Strikes Back): ");
+    const url = `https://swapi.dev/api/films/${id}`;
+
+    try {
+        console.log("\nConsultando Arquivos Cinematográficos...");
+
+        const resposta = await axios.get(url);
+        const dados = resposta.data;
+        
+        console.log("\n=== Resultado Encontrado ===");
+        console.log(`Filme: ${dados.title}`);
+        console.log(`Diretor: ${dados.director}`);
+        console.log(`Produtor: ${dados.producer}`);
+        console.log(`Data de Lançamento: ${dados.release_date}`);
+        console.log(`Descrição do Filme:\n${dados.opening_crawl}`)
+        console.log("-----------------------------------");
+
+    } catch (erro) {
+        console.log("[Erro]: ID do Filme não encontrado. Tente novamente.");
+    }
+}
+
+//função para consultar as espécies
+async function consultarEspecies() {
+    const id = prompt("Digite o ID da Espécie (ex: 1 para Humano): ");
+    const url = `https://swapi.dev/api/species/${id}`;
+
+    try {
+        console.log("\nVerificando informações sobre as Espécies...");
+
+        const resposta = await axios.get(url);
+        const dados = resposta.data;
+
+        console.log("\n=== Resultado Encontrado ===");
+        console.log(`Espécie: ${dados.name}`);
+        console.log(`Classificação: ${dados.classification}`);
+        console.log(`Designação: ${dados.designation}`);
+        console.log(`Altura Média da Espécie: ${dados.average_heigth}`);
+        console.log(`Expectativa de Vida da Espécie: ${dados.average_lifespan}`);
+        console.log(`Língua falada pela Espécie: ${dados.language}`);
+        console.log(`Cor da Pele da Espécia: ${dados.skin_colors}`);
+        console.log(`Cor do Cabelo da Espécie: ${dados.hair_colors}`);
+        console.log(`Cor dos Olhos da Espécie: ${dados.eye_colors}`);
+        console.log("----------------------------");
+    } catch (erro) {
+        console.log("[Erro]: ID da Espécie não encontrada. Tente novamente.");
+    }
+}
+
+//função para consultar veículos
+async function consultarVeiculos() {
+    const id = prompt("Digite o ID do Veículo (ex: 1 para Sand Crawler): ");
+    const url = `https://swapi.dev/api/vehicles/${id}`;
+
+    try {
+        console.log("\nAnalisando Veículos disponíveis...");
+
+        const resposta = await axios.get(url);
+        const dados = resposta.data;
+
+        console.log(`Veículo: ${dados.name}`);
+
+    } catch (erro) {
+        console.log("[Erro]: ID da Espécie não encontrada. Tente novamente.");
+    }
+}
 
 //função principal main
 async function main () {
     let escolha;
-    do {
-        menu();
-
+    do {menu();
         escolha = prompt("Escolha uma opção: ");
         switch (escolha) {
             case "1":
@@ -83,19 +150,19 @@ async function main () {
                 await consultarPlaneta();
                 break;
             case "3":
-                //aqui vai ficar a função para os filmes
+                await consultarFilmes();
                 break;
             case "4":
-                //aqui vai ficar a função para as especies
+                await consultarEspecies();
                 break;
             case "5":
-                //aqui vai ficar a função para os veículos
+                await consultarVeiculos();
                 break;
             case "6":
                 //aqui vai ficar a função para as naves 
                 break;
             case "7":
-                console.log("\nSaindo da API  de Star Wars... Que a força esteja com você!")
+                console.log("\nSaindo da API de Star Wars... Que a força esteja com você!")
                 break;
             default: 
                 console.log("\n[Erro]: Escolha Inválida.")
